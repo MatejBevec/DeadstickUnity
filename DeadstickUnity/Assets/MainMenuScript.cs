832c7ccef655d96bf8f2f3dadc7e1c45f183bb5f
Jan Krnc <jan@krnc.si>
2019-01-04 17:45:00 +0100

    MainMenu completed

---
+++ DeadstickUnity/Assets/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void toMenu2(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void toHighScores()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
