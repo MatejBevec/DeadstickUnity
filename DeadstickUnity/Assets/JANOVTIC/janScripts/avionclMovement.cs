bf38a0e60350c48f8b60816d746ed29ec9975508
Jan Krnc <jan@krnc.si>
2019-01-04 18:31:14 +0100

    lahko pavziramo

--- DeadstickUnity/Assets/JANOVTIC/janScripts/avionclMovement.cs
+++ DeadstickUnity/Assets/JANOVTIC/janScripts/avionclMovement.cs
@@ -20,10 +20,12 @@ public class avionclMovement : MonoBehaviour
     public float dragPower; //večji hitreje se ustavi ko ne držimo pogona
     public float weight;  //večji hitreje pada, težje se dvigne
 
-    public float gravity; //=pospešekmanjsi ko je vecji je
+    public float gravity; //= težni pospešek   (manjsi ko je vecji je)
 
     private Vector3 thrust;
     private Vector3 vecDown;
+
+    private bool pauseGame = false;
 
     void Start()
     {
@@ -36,72 +38,85 @@ public class avionclMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speeed = speed;
-
-        //TURN
-        transform.Rotate(turnSpeed / verticalLessThanHorizontal * Input.GetAxis("Vertical"), 0, -turnSpeed * Input.GetAxis("Horizontal"));
-
-        //CALCULATE THRUST
-        thrust = transform.forward * speed * Time.deltaTime;
-
-        //MOVE AVIONCL
-        transform.position += thrust;
-
-        //THRUST
-        if (Input.GetKey("space") && speed < maxSpeed)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            speed *= enginePower;
-        }
-        else if (speed > minSpeed)
-        {
-            speed /= dragPower;
-        }
-
-
-        //PREMALO SPEEDA
-        if (speed < minSpeed)
-        {
-            transform.position += vecDown * ((minSpeed - speed) / (gravity * 2 * weight));
-
-            if (transform.up.y > 0f)
+            if (pauseGame)
             {
-                transform.Rotate((minSpeed - speed) * (weight*2), 0f, 0f);
+                pauseGame = false;
             }
             else
             {
-                transform.Rotate(-(minSpeed - speed) * (weight*2), 0f, 0f);
+                pauseGame = true;
             }
+
         }
 
+        
 
-        //OBNAŠANJE AVIONCLA ZARADI TEŽE
-        if (transform.forward.y > 0f)
+        if (!pauseGame) //če ni pauze se use skp začne
         {
-            speed /= 1f + (transform.forward.y * weight / gravity);
-        }
-        if (transform.forward.y < 0f)
-        {
-            speed *= 1f + -(transform.forward.y) * weight / gravity;
-        }
+            speeed = speed;
+
+            //TURN
+            transform.Rotate(turnSpeed / verticalLessThanHorizontal * Input.GetAxis("Vertical"), 0, -turnSpeed * Input.GetAxis("Horizontal"));
+
+            //CALCULATE THRUST
+            thrust = transform.forward * speed * Time.deltaTime;
+
+            //MOVE AVIONCL
+            transform.position += thrust;
+
+            //THRUST
+            if (Input.GetKey("space") && speed < maxSpeed)
+            {
+                speed *= enginePower;
+            }
+            else if (speed > minSpeed)
+            {
+                speed /= dragPower;
+            }
+
+
+            //PREMALO SPEEDA
+            if (speed < minSpeed)
+            {
+                transform.position += vecDown * ((minSpeed - speed) / (gravity * 2 * weight));
+
+                if (transform.up.y > 0f)
+                {
+                    transform.Rotate((minSpeed - speed) * (weight * 2), 0f, 0f);
+                }
+                else
+                {
+                    transform.Rotate(-(minSpeed - speed) * (weight * 2), 0f, 0f);
+                }
+            }
+
+
+            //OBNAŠANJE AVIONCLA ZARADI TEŽE
+            if (transform.forward.y > 0f)
+            {
+                speed /= 1f + (transform.forward.y * weight / gravity);
+            }
+            if (transform.forward.y < 0f)
+            {
+                speed *= 1f + -(transform.forward.y) * weight / gravity;
+            }
 
 
 
-        //OBAŠANJE AVIONCLA ZARADI VZGONA
-        //TAZAKOMENTIRANI SO VRTENJE OKOLI GLOBALNEGA Y TUD KR UREDU ZA FURAT
+            //OBAŠANJE AVIONCLA ZARADI VZGONA
+            //TAZAKOMENTIRANI SO VRTENJE OKOLI GLOBALNEGA Y TUD KR UREDU ZA FURAT
             if (transform.right.y > 0f)
             {
-               //transform.Rotate(0f, -0.4f * transform.right.y, 0f, Space.World);
-                 transform.Rotate(-wingArea * transform.right.y, -wingArea * turnLift * transform.right.y, 0f);
+                //transform.Rotate(0f, -0.4f * transform.right.y, 0f, Space.World);
+                transform.Rotate(-wingArea * transform.right.y, -wingArea * turnLift * transform.right.y, 0f);
             }
             if (transform.right.y < 0f)
             {
                 transform.Rotate(wingArea * transform.right.y, -wingArea * turnLift * transform.right.y, 0f);
-               // transform.Rotate(0f, -0.4f * transform.right.y, 0f, Space.World);
+                // transform.Rotate(0f, -0.4f * transform.right.y, 0f, Space.World);
             }
-      
-
-
-
-
+        }
     }
 }
