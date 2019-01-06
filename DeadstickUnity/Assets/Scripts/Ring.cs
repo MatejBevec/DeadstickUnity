6266b89759dbd53944707694e7fe5f8441a3b8ca
MatejBevec <40042371+MatejBevec@users.noreply.github.com>
2019-01-06 16:02:50 +0100

    add ring prefab

---
+++ DeadstickUnity/Assets/Scripts/Ring.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+
+    public bool tagged;
+    public int state; //0-invisible, 1-visible, 2-active
+
+    public Material untaggedMat;
+    public Material taggedMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tagged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tagged)
+        {
+            GetComponent<Renderer>().material = taggedMat;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = untaggedMat;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        tagged = !tagged;
+    }
+}
---
+++ DeadstickUnity/Assets/Scripts/RingManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingManager : MonoBehaviour
+{
+    public List<GameObject> Rings;
+    public GameObject ringPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for testing - generate some rings
+        int numRings = 10;
+        int offset = 10;
+        for (int i = 0; i < numRings; i++)
+        {
+            int x = Random.Range(-offset,offset);
+            int y = Random.Range(-offset, offset);
+            GameObject newRing  = Instantiate(ringPrefab, new Vector3(x,y,(i+1)*50), Quaternion.identity);
+            newRing.name = "Ring" + i;
+            Rings.Add(newRing);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //return pointer to the ring at given index
+    public GameObject FindRing(int index)
+    {
+        return Rings[index];
+    }
+
+}
