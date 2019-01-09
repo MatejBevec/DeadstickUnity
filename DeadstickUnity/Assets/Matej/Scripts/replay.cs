6f3bc48d1e1b1ca35c9795a765cd0adc137fdd4d
matejzan <45387132+matejzan@users.noreply.github.com>
2019-01-09 20:22:23 +0100

    Added ghost script
    record script for original game object, replay for ghost

---
+++ DeadstickUnity/Assets/Matej/Scripts/replay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class replay : MonoBehaviour
+{
+	
+
+    void Start()
+    {
+ 
+    }
+
+    void FixedUpdate()
+    {
+    		if (record.isFinal == true) { //če imamo kopijo za ghosta
+        transform.position = record.positionsFinal[record.a]; //premika se po pozicijah kopije
+        transform.rotation = record.rotationsFinal[record.a]; //rotira po rotacijah kopije
+        
+    	}
+    }
+}
+
+
+
